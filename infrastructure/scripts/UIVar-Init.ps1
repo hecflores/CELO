f0f41eda815d1f8f7253bba7a10f50aa9483e764
Hector Flores <hectorhpflores72@gmail.com>
2018-02-11 17:55:21 -0600

    Fixed Regex

--- infrastructure/scripts/UIVar-Init.ps1
+++ infrastructure/scripts/UIVar-Init.ps1
@@ -3,7 +3,6 @@ $env:DatabaseName = $env:DatabaseName -replace "[^\w\d_]",""
 Write-Host "##vso[task.setvariable variable=DatabaseName]$env:DatabaseName"
 
 $env:Host = $env:Host -replace "refs\/heads\/",""
-$env:Host = $env:Host -replace "[\/]","-"
 Write-Host "##vso[task.setvariable variable=Host]$env:Host"
 
 $date = Get-Date -Format g 
