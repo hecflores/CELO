e5329f6dc6c5fa0d7dfe10c36291cf41ce022a49
hectorhpflores72 <hectorhpflores72@gmail.com>
2019-04-22 17:09:56 +0000

    Pushing again but keeping Files

--- UI/Backend/module/Files/config/module.config.php
+++ UI/Backend/module/Files/config/module.config.php
@@ -12,8 +12,6 @@ namespace Application;
 return array(
     'router' => array(
         'routes' => array(
-
-
             'get-file'=>array(
                 'type' => 'segment',
                 'options' => array(
@@ -31,104 +29,9 @@ return array(
                         ),
                         'title'=>"Get File"
                     ),
-                ),
-
-            ),
-//            'upload-file'=>array(
-//
-//            ),
-
-
-
-            /***************************************************************/
-            /*                     J s o n   A P I                         */
-            /***************************************************************/
-            'course-content-create' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/:instructor/section/[:sectionID]/course-content/create',
-                    'constraints' => array(
-                        "sectionID"=>'[a-zA-Z0-9_-]+',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'Files\Controller\Index',
-                        'action'     => 'create',
-                        'roles' => array(
-                            Roles::$ADMIN,
-                            Roles::$INSTRUCTOR,
-                            Roles::$TA,
-                        ),
-                        'title'=>"Content Create"
-                    ),
-                ),
-            ),
-
-            'course-content' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route'    => '/[:role]/section/[:sectionID]/course-content/[:contentID]',
-                    'constraints' => array(
-                        'contentID' => '[0-9]+',
-                        "sectionID"=>'[a-zA-Z0-9_-]+',
-                        "role"=>'administrator|student|ta|instructor'
-                    ),
-                    'defaults' => array(
-                        'controller' => 'Instructor\Controller\User',
-                        'action'     => 'sectionHome',
-                        'roles' => array(
-                            Roles::$INSTRUCTOR,
-                            Roles::$STUDENT,
-                            Roles::$TA,
-                            Roles::$INSTRUCTOR,
-                        ),
-                        'title'=>"Course Content"
-                    ),
-                ),
-
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // Segment route for viewing one blog post
-
-                    'view' => array(
-                        'type' => 'literal',
-                        'options' => array(
-                            'route' => '/view',
-                            'defaults' => array(
-                                'controller' => 'Files\Controller\Index',
-                                'action'     => 'view',
-                                'roles' => array(
-                                    Roles::$ADMIN,
-                                    Roles::$INSTRUCTOR,
-                                    Roles::$TA,
-                                    Roles::$STUDENT,
-                                ),
-                                'title'=>"Content View"
-                            ),
-                        ),
-                    ),
-
-                    'run' => array(
-                        'type' => 'literal',
-                        'options' => array(
-                            'route' => '/run',
-                            'defaults' => array(
-                                'controller' => 'Files\Controller\Index',
-                                'action'     => 'run',
-                                'roles' => array(
-                                    Roles::$ADMIN,
-                                    Roles::$INSTRUCTOR,
-                                    Roles::$TA,
-                                    Roles::$STUDENT,
-                                ),
-                                'title'=>"Content Run"
-                            ),
-                        ),
-                    ),
-                ),
+                )
             )
         )
-
-
     ),
     'service_manager' => array(
         'abstract_factories' => array(
